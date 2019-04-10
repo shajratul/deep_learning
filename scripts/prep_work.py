fc9713cf8090bca860a49ca37327a3b11081a73d
shajratul <46979766+shajratul@users.noreply.github.com>
2019-04-10 12:26:13 -0400

    Add files via upload

---
+++ scripts/prep_work.py
@@ -0,0 +1,15 @@
+import cv2
+import glob
+
+# Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
+try:
+   del EOSINOPHIL,LYMPHOCYTE
+   del MONOCYTE, NEUTROPHIL, All_cell
+   print('Clear previously loaded data.')
+except:
+   pass
+
+path = "/data"
+EOSINOPHIL = [cv2.imread(file) for file in glob.glob(path+"/*.jpeg")]
+sam_eosino = len(EOSINOPHIL)
+print(sam_eosino)
